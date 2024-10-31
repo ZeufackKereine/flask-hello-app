a656661832f81feda22e8558fec5ddc335c73fa3
unknown <zeufackkereine@gmail.com>
2024-10-31 17:27:04 +0100

    zeufack

--- app.py
+++ app.py
@@ -7,20 +7,21 @@ app = Flask(__name__)
 # Define the route
 @app.route('/')
 def home():
-  return jsonify(message="Hello level 400 FET, Quality Assurance!")
+    return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
 # Define the unit test class
 class TestMyApp(unittest.TestCase):
-
-  # Set up a test client
-  def setUp(self):
-    self.app = app.test_client()
+  
+ # Set up a test client
+    def setUp(self):
+        self.app = app.test_client()
 
   # Test the home route
-  def test_home_route(self):
-    response = self.app.get('/')
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.json, {'message': 'Hello level 400 FET, Quality Assurance!'})
+    def test_home_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'message': 'Hello level 400 FET, Quality Assurance!'})
 
-if __name__ == '__main__':
-  unittest.main()
+    if __name__ == '__main__':
+        app.run(debug=True)
+        unittest.main()
